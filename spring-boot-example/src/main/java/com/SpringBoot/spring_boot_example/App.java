294d02c27c8be332ff0db040f1010c63b5d1ae76
prathu <prathu>
2024-05-08 23:52:57 +0530

    testing new commit

---
+++ spring-boot-example/src/main/java/com/SpringBoot/spring_boot_example/App.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.spring_boot_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
